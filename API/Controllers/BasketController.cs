b31262bd0f492c9337fb23b75ed0aaa52782668b
Nathan G <Nathanglass26@gmail.com>
2023-04-18 21:42:15 +0100

    basket controller

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,19 @@
+using API.Data;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BasketController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await _context.Baskets
+        }
+    }
+}
